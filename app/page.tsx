a936bea96b088ee60555031a65c3ecea3acc9311
kent1988hk <kent.law.production01@gmail.com>
2026-02-01 19:49:13 +0000

    implement cocktail section

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@ import { useGSAP } from "@gsap/react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
 import Hero from "@/components/Hero";
+import Cocktails from "@/components/Cocktails";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 export default function Home() {
   return (
     <main className="flex-center flex-col">
       <Hero />
+      <Cocktails />
     </main>
   );
 }
