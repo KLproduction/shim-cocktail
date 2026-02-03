cdca9fb46f29f4036140583a8699de3947dc48a5
kent1988hk <kent.law.production01@gmail.com>
2026-02-03 17:06:27 +0000

    implement Art section

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import gsap from "gsap";
 import Hero from "@/components/Hero";
 import Cocktails from "@/components/Cocktails";
 import About from "@/components/About";
+import Art from "@/components/Art";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 export default function Home() {
@@ -12,6 +13,7 @@ export default function Home() {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
     </main>
   );
 }
